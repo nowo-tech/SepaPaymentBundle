64de9e1c7b3a657d6baa93018a6e4150c8835699
GitHub Action <action@github.com>
2026-01-09 08:57:37 +0000

    Apply PHP CS Fixer fixes [skip ci]

--- src/Generator/RemesaGenerator.php
+++ src/Generator/RemesaGenerator.php
@@ -442,12 +442,10 @@ class RemesaGenerator
      * This ensures addresses are included even if the library doesn't support them directly.
      *
 <<<<<<< HEAD
-     * @param string      $xml        The generated XML
-     * @param RemesaData  $remesaData The credit transfer data with addresses
-=======
+     * @param string     $xml        The generated XML
+     * @param RemesaData $remesaData The credit transfer data with addresses
      * @param string     $xml        The generated XML
      * @param RemesaData $remesaData The remesa data with addresses
->>>>>>> 179ca0733d9563c7a77fb272b7861da5d301b8c6
      *
      * @return string The XML with addresses added
      */
